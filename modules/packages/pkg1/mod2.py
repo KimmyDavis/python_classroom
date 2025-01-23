79e886c1fce0a48451d9c6d75b30a57833c40ed3
Davis <kimdhev@gmail.com>
2025-01-23 02:14:25 +0100

    renamed info.md to README.md in modules for display issues on github, save an unsaved file in modules and created a new directory for classes and OOP with a README.md file

--- modules/packages/pkg1/mod2.py
+++ modules/packages/pkg1/mod2.py
@@ -1,5 +1,6 @@
-x = 25 
-print("in mod2 inside pkg1", "_"*100)
+x = 25
+from consts import mod_heading_line
+print("in mod2 inside pkg1", mod_heading_line)
 
 from . import mod1
 print("mod2 prints mod1's x ->",mod1.x)
